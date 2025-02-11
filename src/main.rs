fe5eb93046a548ae5fcfc419f6e5f8d02ad4d168
Rob Blackbourn <rob.blackbourn@gmail.com>
2025-02-11 07:15:48 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use ldap3::{LdapConnAsync, LdapConnSettings};
+use std::io;
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    let url = "ldap://localhost:2389";
+    let username = "jane@example.com";
+    let password = "password";
+    let is_authenticated = authenticate(&url, &username, &password).await?;
+
+    println!("{} authenticated {}", username, is_authenticated);
+
+    Ok(())
+}
+
+// Pass in username and password to authenticate against LDAP
+async fn authenticate(url: &str, username: &str, password: &str) -> io::Result<bool> {
+    // Connection to the LDAP Server
+    let (conn, mut ldap) = LdapConnAsync::with_settings(
+        LdapConnSettings::new()
+            .set_starttls(true)
+            .set_no_tls_verify(true),
+        url,
+    )
+    .await?;
+    ldap3::drive!(conn);
+
+    // Attempts a simple bind using the passed in values of username and Password
+    let result = ldap
+        .simple_bind(username, password)
+        .await?
+        .success();
+    ldap.unbind().await?;
+
+    return Ok(result.is_err())
+}
